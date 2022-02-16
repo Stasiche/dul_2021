408dac7d1e69771b9cc3d7ac761eb7159a2c3306
Stanislav_Lebedev <lebedevsf239@gmail.com>
2022-02-16 16:53:58 +0300

    first

---
+++ src/net.py
@@ -0,0 +1,42 @@
+from torch import nn
+from torch.nn import functional as F
+
+
+class Net(nn.Module):
+    def __init__(self, out_dim=128, hid_dim_full=128):
+        super(Net, self).__init__()
+
+        self.conv1 = nn.Conv2d(1, 16, 5, padding=2)
+        self.conv2 = nn.Conv2d(16, 16, 3, padding=1, stride=2)
+        self.conv3 = nn.Conv2d(16, 32, 5, padding=2)
+        self.conv4 = nn.Conv2d(32, 32, 3, padding=1, stride=2)
+        self.conv5 = nn.Conv2d(32, 32, 1)
+        self.conv6 = nn.Conv2d(32, 4, 1)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.bn2 = nn.BatchNorm2d(16)
+        self.bn3 = nn.BatchNorm2d(32)
+        self.bn4 = nn.BatchNorm2d(32)
+        self.bn5 = nn.BatchNorm2d(32)
+        self.bn6 = nn.BatchNorm2d(4)
+
+        self.conv_to_fc = 4 * 6 * 6
+        self.fc1 = nn.Linear(self.conv_to_fc, hid_dim_full)
+        self.fc2 = nn.Linear(hid_dim_full, int(hid_dim_full // 2))
+
+        self.features = nn.Linear(int(hid_dim_full // 2), out_dim)
+
+    def forward(self, x):
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = F.relu(self.bn4(self.conv4(x)))
+        x = F.relu(self.bn5(self.conv5(x)))
+        x = F.relu(self.bn6(self.conv6(x)))
+
+        x = x.view(-1, self.conv_to_fc)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+
+        features = self.features(x)
+
+        return features
